9117349d0e906d29fb58f7ab17ce737fefa78b05
Askhat <baltabaev.a2509@gmail.com>
2020-06-13 17:18:35 +0600

    wfe

--- src/controllers/parser.tsx
+++ src/controllers/parser.tsx
@@ -1,6 +1,6 @@
 import request from '../configs/instamartApi';
 
 export const getShop = async (instagramLogin: string): Promise<any> => {
-  const response = await request.get(`/instaparser/${instagramLogin}`);
+  const response = await request.get(`/instaparser/${instagramLogin}/30`);
   return response.data;
 };
